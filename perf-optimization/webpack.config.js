2950274294d7aa2d6b8fad34aeb5ed2d1ce6d0a4
jser_dimple <2274171398@qq.com>
2018-09-20 23:23:25 +0800

    if there was some errors ,Please make perf-optimization as test

---
+++ perf-optimization/webpack.config.js
@@ -0,0 +1,55 @@
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'app/index.jsx'),
+    output: {
+        filename: "bundle.js"
+    },
+
+    resolve:{
+        extensions:['', '.js','.jsx']
+    },
+
+    module: {
+        loaders: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
+            { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less' },
+            { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss' },
+            { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000' },  // 限制大小5kb
+            { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000'} // 限制大小小于5k
+        ]
+    },
+
+    postcss: [
+        require('autoprefixer') 
+    ],
+
+    plugins: [
+       
+        new HtmlWebpackPlugin({
+            template: __dirname + '/app/index.tmpl.html'
+        }),
+
+
+        new webpack.HotModuleReplacementPlugin(),
+
+        // 打开浏览器
+        new OpenBrowserPlugin({
+          url: 'http://localhost:8080'
+        }),
+
+        new webpack.DefinePlugin({
+          __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+        })
+    ],
+
+    devServer: {
+        colors: true,
+        historyApiFallback: true,
+        inline: true, 
+        hot: true  // 使用热加载插件 HotModuleReplacementPlugin
+    }
+}
