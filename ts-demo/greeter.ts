25342b2b136702a3cb3af4b47140eb48aff1f92c
jser_dimple <2274171398@qq.com>
2018-09-25 23:09:25 +0800

    ts

---
+++ ts-demo/greeter.ts
@@ -0,0 +1,19 @@
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Dimple", "jser.", "User");
+
+document.body.innerHTML = greeter(user);
